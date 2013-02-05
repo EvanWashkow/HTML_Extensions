9c9fa72cd900314a5c028ebd164d04199c343af7
Evan Washkow <epwashkow@gmail.com>
2013-02-04 16:12:04 -0800

    Tab Manager: setting the active tab's class to "active"
    This will allow tab styling. An example is shown in the example.html

--- tab-manager/tab-manager.js
+++ tab-manager/tab-manager.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
+
 	$('.tab-wrapper > *').click(function () {
-		// Hide all content containers
+
+		// Remove active class from all tabs and tab contents (this will remove selected tab styling and hide all content)
+		$('.tab-wrapper > *').removeClass('active');
 		$('.content-wrapper > *').removeClass('active');
 
-		// Shaw the corresponding content
+		// Set the clicked tab as active (this will style the selected tab)
+		$(this).addClass('active');
+
+		// Set the tab's corresponding content to active (this will show the content)
 		var contentId = $(this).attr('content-id');
 		$('#' + contentId).addClass('active');
 	});
