3b556fc1af8dd833595bbaf1041a8e54c725a94c
EvanWashkow <epwashkow@gmail.com>
2013-05-05 18:57:10 -0700

    Encapsulating tab switching into its own function. Adding function callbacks (before and after switching tabs)

--- tab-manager/tab-manager.js
+++ tab-manager/tab-manager.js
@@ -1,16 +1,30 @@
+// Function callbacks. These receive the object of the new tab as a parameter.
+var doBeforeTabSwitch;
+var doAfterTabSwitch;
+
 $(document).ready(function () {
-
 	$('.tab-wrapper > *').click(function () {
-
-		// Remove active class from all tabs and tab contents (this will remove selected tab styling and hide all content)
-		$('.tab-wrapper > *').removeClass('active');
-		$('.content-wrapper > *').removeClass('active');
-
-		// Set the clicked tab as active (this will style the selected tab)
-		$(this).addClass('active');
-
-		// Set the tab's corresponding content to active (this will show the content)
-		var contentId = $(this).attr('content-id');
-		$('#' + contentId).addClass('active');
+		switchTabTo(this);
 	});
 });
+
+function switchTabTo(newTab) {
+	// Perform needed actions before switching tabs
+	if (typeof doBeforeTabSwitch === 'function')
+		doBeforeTabSwitch(this);
+
+	// Remove active class from all tabs and tab contents (this will remove selected tab styling and hide all content)
+	$('.tab-wrapper > *').removeClass('active');
+	$('.content-wrapper > *').removeClass('active');
+
+	// Set the clicked tab as active (this will style the selected tab)
+	$(newTab).addClass('active');
+
+	// Set the tab's corresponding content to active (this will show the content)
+	var contentId = $(newTab).attr('content-id');
+	$('#' + contentId).addClass('active');
+
+	// Perform needed actions after switching tabs.
+	if (typeof doAfterTabSwitch === 'function')
+		doAfterTabSwitch(this);
+}
