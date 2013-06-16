79792b0a827e8004fe6c8a6528b9757a1c1183e8
EvanWashkow <epwashkow@gmail.com>
2013-06-15 20:33:01 -0700

    Removing unneccesary spaces

--- php/url_helper/index.php
+++ php/url_helper/index.php
@@ -1,5 +1,5 @@
 <?php
-	include('url_helper.php');
-	include(url_helper('/index.php'));
-	include(url_helper('test.php'));
+include('url_helper.php');
+include(url_helper('/index.php'));
+include(url_helper('test.php'));
 ?>
