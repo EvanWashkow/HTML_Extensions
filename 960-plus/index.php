4b4d34a70fc54fc3a76ad33499845c8b4970a5cf
EvanWashkow <epwashkow@gmail.com>
2013-09-13 10:36:26 -0700

    Setting up the standard 960 24 column grid system

---
+++ 960-plus/index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- 24 Column Grid -->
+		<link rel="stylesheet" href="css/960_24_col.css">
+
+		<!-- FOR DEMO ONLY -->
+		<link rel="stylesheet" href="css/style.css">
+
+		<title>960 Plus Grid System &mdash; Demo</title>
+	</head>
+	<body>
+		<h1>960 Plus Grid System</h1>
+		<div class="container_24">
+		  <h2>
+		    24 Column Grid
+		  </h2>
+		  <div class="grid_24">
+		    <p>
+		      950
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="grid_23">
+		    <p>
+		      910
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_2">
+		    <p>
+		      70
+		    </p>
+		  </div>
+		  <div class="grid_22">
+		    <p>
+		      870
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_3">
+		    <p>
+		      110
+		    </p>
+		  </div>
+		  <div class="grid_21">
+		    <p>
+		      830
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_4">
+		    <p>
+		      150
+		    </p>
+		  </div>
+		  <div class="grid_20">
+		    <p>
+		      790
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_5">
+		    <p>
+		      190
+		    </p>
+		  </div>
+		  <div class="grid_19">
+		    <p>
+		      750
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_6">
+		    <p>
+		      230
+		    </p>
+		  </div>
+		  <div class="grid_18">
+		    <p>
+		      710
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_7">
+		    <p>
+		      270
+		    </p>
+		  </div>
+		  <div class="grid_17">
+		    <p>
+		      670
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_8">
+		    <p>
+		      310
+		    </p>
+		  </div>
+		  <div class="grid_16">
+		    <p>
+		      630
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_9">
+		    <p>
+		      350
+		    </p>
+		  </div>
+		  <div class="grid_15">
+		    <p>
+		      590
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_10">
+		    <p>
+		      390
+		    </p>
+		  </div>
+		  <div class="grid_14">
+		    <p>
+		      550
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_11">
+		    <p>
+		      430
+		    </p>
+		  </div>
+		  <div class="grid_13">
+		    <p>
+		      510
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_12">
+		    <p>
+		      470
+		    </p>
+		  </div>
+		  <div class="grid_12">
+		    <p>
+		      470
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 suffix_23">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_1 suffix_22">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_2 suffix_21">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_3 suffix_20">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_4 suffix_19">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_5 suffix_18">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_6 suffix_17">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_7 suffix_16">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_8 suffix_15">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_9 suffix_14">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_10 suffix_13">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_11 suffix_12">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="grid_1 prefix_12 suffix_11">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_13 suffix_10">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_14 suffix_9">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_15 suffix_8">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_16 suffix_7">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_17 suffix_6">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_18 suffix_5">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_19 suffix_4">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_20 suffix_3">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_21 suffix_2">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_22 suffix_1">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_1 prefix_23">
+		    <p>
+		      30
+		    </p>
+		  </div>
+		  <div class="clear"></div>
+		  <div class="grid_12 push_12">
+		    <div class="grid_6 alpha">
+		      <p>
+		        230
+		      </p>
+		    </div>
+		    <div class="grid_6 omega">
+		      <p>
+		        230
+		      </p>
+		    </div>
+		    <div class="clear"></div>
+		    <div class="grid_1 alpha">
+		      <p>
+		        30
+		      </p>
+		    </div>
+		    <div class="grid_11 omega">
+		      <p>
+		        430
+		      </p>
+		    </div>
+		  </div>
+		  <div class="grid_12 pull_12">
+		    <div class="grid_1 alpha">
+		      <p>
+		        30
+		      </p>
+		    </div>
+		    <div class="grid_11 omega">
+		      <p>
+		        430
+		      </p>
+		    </div>
+		    <div class="clear"></div>
+		    <div class="grid_6 alpha">
+		      <p>
+		        230
+		      </p>
+		    </div>
+		    <div class="grid_6 omega">
+		      <p>
+		        230
+		      </p>
+		    </div>
+		  </div>
+		</div>
+	</body>
+</html>
