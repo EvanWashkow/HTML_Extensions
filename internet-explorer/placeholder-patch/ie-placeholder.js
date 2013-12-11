051fed9ed98ae9e5645928589d94c6ee55d9b51d
Evan Washkow <evan.washkow@logos.com>
2013-12-11 12:31:15 -0800

    Fixing tab indentation

--- internet-explorer/placeholder-patch/ie-placeholder.js
+++ internet-explorer/placeholder-patch/ie-placeholder.js
@@ -1,18 +1,18 @@
 /* All you have to do is include this into your page. The script will take care of making IE compatible with any element that is using placeholders. */
 
 window.onload = function () {
-  if (navigator.appName === 'Microsoft Internet Explorer') {
-    $('body [placeholder]').each(function (index) {
-      $(this)
-        .attr('value', $(this).attr('placeholder'))
-        .focusin(function () {
-          if ($(this).attr('value') === $(this).attr('placeholder'))
-            $(this).attr('value', '');
-        })
-        .focusout(function () {
-          if ($(this).attr('value') === '')
-            $(this).attr('value', $(this).attr('placeholder'));
-        });
-    });
-  }
+	if (navigator.appName === 'Microsoft Internet Explorer') {
+		$('body [placeholder]').each(function (index) {
+			$(this)
+				.attr('value', $(this).attr('placeholder'))
+				.focusin(function () {
+					if ($(this).attr('value') === $(this).attr('placeholder'))
+						$(this).attr('value', '');
+				})
+				.focusout(function () {
+					if ($(this).attr('value') === '')
+						$(this).attr('value', $(this).attr('placeholder'));
+				});
+		});
+	}
 }
