76e60ac17dd51a95eb09f711b3a239aee7094625
Evan Washkow <evan.washkow@logos.com>
2013-12-11 15:48:31 -0800

    Removing jQuery requirement

--- internet-explorer/placeholder-patch/index.php
+++ internet-explorer/placeholder-patch/index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<title>IE Placeholder patch Demo</title>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.2.3/jquery.min.js"></script>
 	<script src="ie-placeholder.js"></script>
 </head>
 <body>
