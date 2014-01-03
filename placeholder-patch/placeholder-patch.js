7b16f170b24f2383a1c0d8468052e170d4454fed
Evan Washkow <evan.washkow@logos.com>
2014-01-03 14:56:53 -0800

    Object.add_() -> add_()

--- placeholder-patch/placeholder-patch.js
+++ placeholder-patch/placeholder-patch.js
@@ -1,14 +1,5 @@
 /* All you have to do is include this into your page. The script will ensure all browsers display a placeholder--even those that don't support it. */
 
-Object.prototype.addPlaceholderPatch = function() {
-	if (!this.getAttribute('value'))
-		this.setAttribute('value', this.getAttribute('placeholder'));
-}
-Object.prototype.removePlaceholderPatch = function() {
-	if (this.getAttribute('value') === this.getAttribute('placeholder'))
-		this.setAttribute('value', '');
-}
-
 window.onload = function () {
 
 	// If the browser does not support placeholder, continue
@@ -16,13 +7,22 @@ window.onload = function () {
 		var placeholderElements = document.querySelectorAll('body [placeholder]');
 		for (var i = placeholderElements.length - 1; i >= 0; i--) {
 			var element = placeholderElements[i];
-			element.addPlaceholderPatch();
+			addPlaceholderPatch(element);
 			element.addEventListener('focus', function() {
-				this.removePlaceholderPatch();
+				removePlaceholderPatch(this);
 			});
 			element.addEventListener('blur', function() {
-				this.addPlaceholderPatch();
+				addPlaceholderPatch(this);
 			});
 		}
 	}
+}
+
+function addPlaceholderPatch(element) {
+	if (!element.getAttribute('value'))
+		element.setAttribute('value', element.getAttribute('placeholder'));
+}
+function removePlaceholderPatch(element) {
+	if (element.getAttribute('value') === element.getAttribute('placeholder'))
+		element.setAttribute('value', '');
 }
