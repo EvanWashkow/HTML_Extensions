2c4b372e06c68dad8abea77280fdd2da0aa1ba08
Evan Washkow <evan.washkow@logos.com>
2014-01-03 13:46:18 -0800

    Modularizing placeholder patch functionality

--- placeholder-patch/placeholder-patch.js
+++ placeholder-patch/placeholder-patch.js
@@ -1,5 +1,14 @@
 /* All you have to do is include this into your page. The script will ensure all browsers display a placeholder--even those that don't support it. */
 
+Object.prototype.addPlaceholderPatch = function() {
+	if (!this.getAttribute('value'))
+		this.setAttribute('value', this.getAttribute('placeholder'));
+}
+Object.prototype.removePlaceholderPatch = function() {
+	if (this.getAttribute('value') === this.getAttribute('placeholder'))
+		this.setAttribute('value', '');
+}
+
 window.onload = function () {
 
 	// If the browser does not support placeholder, continue
@@ -7,14 +16,12 @@ window.onload = function () {
 		var placeholderElements = document.querySelectorAll('body [placeholder]');
 		for (var i = placeholderElements.length - 1; i >= 0; i--) {
 			var element = placeholderElements[i];
-			element.setAttribute('value', placeholderElements[i].getAttribute('placeholder'));
+			element.addPlaceholderPatch();
 			element.addEventListener('focus', function() {
-				if (this.getAttribute('value') === this.getAttribute('placeholder'))
-					this.setAttribute('value', '');
+				this.removePlaceholderPatch();
 			});
 			element.addEventListener('blur', function() {
-				if (this.getAttribute('value') === '')
-					this.setAttribute('value', this.getAttribute('placeholder'));
+				this.addPlaceholderPatch();
 			});
 		}
 	}
